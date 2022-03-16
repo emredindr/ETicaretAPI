45a4ad702a87d27c67ef3315bad86ff613f756ca
emredindr <emredindr@gmail.com>
2022-03-16 18:28:21 +0300

    Onion Architecture Altyapısında İnşa

---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Persistence.Contexts;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceServices(this IServiceCollection services) 
+        {
+            services.AddDbContext<ETicaretAPIDbContext>(option =>option.UseNpgsql("User ID=postgres;Password=emre77;Host=localhost;Port=5432;Database=ETicaretAPIDb;"));
+           
+
+        } 
+    }
+}
